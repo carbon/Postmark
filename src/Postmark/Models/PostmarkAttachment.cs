b66c28f5e25aa968ed57473bb87146e4ec87454a
Jason Nelson <jason@carbonmade.com>
2019-02-20 13:25:01 -0600

    Add constructor to PostmarkAttachment

--- src/Postmark/Models/PostmarkAttachment.cs
+++ src/Postmark/Models/PostmarkAttachment.cs
@@ -1,11 +1,25 @@
-﻿namespace Postmark
+﻿using System.Runtime.Serialization;
+
+namespace Postmark
 {
     public class PostmarkAttachment
     {
+        public PostmarkAttachment() { }
+
+        public PostmarkAttachment(string name, byte[] content, string contentType)
+        {
+            Name = name;
+            Content = content;
+            ContentType = contentType;
+        }
+
+        [DataMember(Name = "Name")]
         public string Name { get; set; }
 
+        [DataMember(Name = "Content")]
         public byte[] Content { get; set; }
 
+        [DataMember(Name = "ContentType")]
         public string ContentType { get; set; }
     }
 }
